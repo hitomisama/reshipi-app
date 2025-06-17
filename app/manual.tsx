e48bd0c3191457ba6a8af40ca45be5c0c3e12d4c
Yisongjing <24aw0105@jec.ac.jp>
2025-06-17 12:30:26 +0900

    配置代码环境

---
+++ app/manual.tsx
@@ -0,0 +1,10 @@
+// app/manual.tsx
+import { View, Text } from 'react-native';
+
+export default function ManualScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>手动输入页面</Text>
+    </View>
+  );
+}
